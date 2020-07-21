5d7bed29c204f539ac205907c29b1a6b5e613dd4
Benzo26 <bugesi.benzo@gmail.com>
2020-07-22 00:28:26 +0300

    create function to get Akhan name in value using Id Elements

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function getAkhanName () {
+    let yearOfBirth = document.getElementById("year-input").nodeValue;
+    let monthOfBirth = Number(document.getElementById(month-input).value);
+    let dayOfBirth = Number(document.getElementById("day-input").value)
+    let gender = document.getElementById("gender")
+}
