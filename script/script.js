4df452c889ccc24775d3074dd7626d7bb26464b6
Benzo26 <bugesi.benzo@gmail.com>
2020-07-22 00:31:54 +0300

    create function to get gender

--- script/script.js
+++ script/script.js
@@ -2,5 +2,14 @@ function getAkhanName () {
     let yearOfBirth = document.getElementById("year-input").nodeValue;
     let monthOfBirth = Number(document.getElementById(month-input).value);
     let dayOfBirth = Number(document.getElementById("day-input").value)
-    let gender = document.getElementById("gender")
+    let genders = document.getElementById("gender")
+
+    //function to get gender
+    function getGender () {
+        for (let gender of genders) {
+            if(gender.checked) {
+                return gender.value;
+            }
+        }
+    }
 }
