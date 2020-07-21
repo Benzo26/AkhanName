292a169d8a5c291d9fde5d6d9a7fd0552d7c5f6e
Benzo26 <bugesi.benzo@gmail.com>
2020-07-21 21:24:15 +0300

    add validation functions

--- script/script.js
+++ script/script.js
@@ -14,4 +14,31 @@ function getGender () {
   }
 
     let myGenderValue = getGender();
-  console.log(myGenderValue);
+    console.log(myGenderValue);
+
+    // validation functions
+  function monthValidator () {
+    if (monthOfBirth < 1 || monthOfBirth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function dayValidator () {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+      if (dayOfBirth > 28 || dayOfBirth < 1) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        return false;
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      return false;
+    } else {
+      return true;
+    }
+  }
